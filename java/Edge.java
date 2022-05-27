a4f7f564c150b268fce6669cbbdc1ca78521e1f1
Matthias Vietz <matthias.vietz@fau.de>
2022-05-27 17:21:11 +0200

    Java: Update NC structure. Added turn (connection) & flow creation in functionality. Added javadoc.

--- java/Edge.java
+++ java/Edge.java
@@ -1,12 +1,42 @@
+import org.networkcalculus.dnc.network.server_graph.Server;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Edge {
-    private String node1, node2;
-    private double bitrate;
-    private double latency;
+    // Implementing the nodes as a list rather than two separate strings enables easier matching later on.
+    private final List<String> nodes = new ArrayList<>(2);
+    private final double bitrate;
+    private final double latency;
+
+    private Server server;
 
     public Edge(String node1, String node2, double bitrate, double latency) {
-        this.node1 = node1;
-        this.node2 = node2;
+        this.nodes.add(node1);
+        this.nodes.add(node2);
+        Collections.sort(this.nodes);   // sort is important for future comparisons
         this.bitrate = bitrate;
         this.latency = latency;
+    }
+
+    public List<String> getNodes() {
+        return nodes;
+    }
+
+    public double getBitrate() {
+        return bitrate;
+    }
+
+    public double getLatency() {
+        return latency;
+    }
+
+    public Server getServer() {
+        return server;
+    }
+
+    public void setServer(Server server) {
+        this.server = server;
     }
 }
---
+++ java/SGService.java
@@ -0,0 +1,48 @@
+import org.networkcalculus.dnc.network.server_graph.Flow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class used to store informations about a smart grid service
+ */
+public class SGService {
+    private final String name;
+    private final String server;
+    private final int bucket_size;
+    private final int bitrate;
+    private final List<List<String>> multipath;
+    private final List<Flow> flows = new ArrayList<>();
+
+    public SGService(String name, String servername, int bucket_size, int bitrate, List<List<String>> multipath) {
+        this.name = name;
+        this.server = servername;
+        this.bucket_size = bucket_size;
+        this.bitrate = bitrate;
+        this.multipath = multipath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public int getBucket_size() {
+        return bucket_size;
+    }
+
+    public int getBitrate() {
+        return bitrate;
+    }
+
+    public List<List<String>> getMultipath() {
+        return multipath;
+    }
+
+    public void addFlow(Flow flow) {
+        flows.add(flow);
+    }
+}
